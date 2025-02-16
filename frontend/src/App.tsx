27c3a334a0673e67fe56dd7569d0a1a5f4870f32
KSAplay <kennysaavedraaltuna@outlook.com>
2025-02-16 17:49:36 +0100

    Agregar configuración de Tailwind, componentes y utilidades para mejorar el estilo y la estructura del proyecto
    Signed-off-by: KSAplay <kennysaavedraaltuna@outlook.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { Button } from "./components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
 function App() {
   return (
     <>
-      <h1 className="">Hola Mundo</h1>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button className="cursor-pointer">Iniciar Sesión</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <h1 className="text-2xl text-red-400">Hola Mundo</h1>
     </>
   );
 }
