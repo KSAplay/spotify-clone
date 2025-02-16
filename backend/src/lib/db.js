dddd2cf08718b395368d36f872bd887419fb4e40
KSAplay <kennysaavedraaltuna@outlook.com>
2025-02-16 18:29:03 +0100

    Agregar rutas para autenticación, usuarios, administración, canciones, álbumes y estadísticas en el backend
    Signed-off-by: KSAplay <kennysaavedraaltuna@outlook.com>

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Connected to MongoDB: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Database connection failed");
+    process.exit(1); // 1 means error, 0 means success
+  }
+};
