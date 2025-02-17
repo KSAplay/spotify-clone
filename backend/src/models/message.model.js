e74dd3a646883f768af8c1e86b79d928b9e01dd5
KSAplay <kennysaavedraaltuna@outlook.com>
2025-02-17 01:55:30 +0100

    Agregar modelos de Mongoose para álbumes, canciones, mensajes y usuarios
    Signed-off-by: KSAplay <kennysaavedraaltuna@outlook.com>

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    // Clerk user ID
+    senderId: {
+      type: String,
+      required: true,
+    },
+    // Clerk user ID
+    receiverId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Message = mongoose.model("Message", messageSchema);
