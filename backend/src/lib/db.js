92cbbefda9547ec964ab04f647be23deedc4f400
KSAplay <kennysaavedraaltuna@outlook.com>
2025-02-17 02:29:26 +0100

    Agregar controladores para autenticación y administración, mejorar manejo de errores y actualizar .gitignore para variables de entorno
    Signed-off-by: KSAplay <kennysaavedraaltuna@outlook.com>

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,7 @@ export const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Connected to MongoDB: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Database connection failed");
+    console.error("Database connection failed", error);
     process.exit(1); // 1 means error, 0 means success
   }
 };
