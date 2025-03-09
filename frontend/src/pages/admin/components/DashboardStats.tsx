eda0dd34ae84db5f5d12e3e913ebd4f1dff83997
KSAplay <kennysaavedraaltuna@outlook.com>
2025-03-09 01:10:14 +0100

    Traducir etiquetas de estadísticas en el componente DashboardStats a español y corregir error de tipeo en el iconColor de ListMusic
    Signed-off-by: KSAplay <kennysaavedraaltuna@outlook.com>

--- frontend/src/pages/admin/components/DashboardStats.tsx
+++ frontend/src/pages/admin/components/DashboardStats.tsx
@@ -8,28 +8,28 @@ const DashboardStats = () => {
   const statsData = [
     {
       icon: ListMusic,
-      label: "Total Songs",
+      label: "Canciones Totales",
       value: stats.totalSongs.toString(),
       bgColor: "bg-emerald-500/10",
-      iconColor: "tsext-emerald-500",
+      iconColor: "text-emerald-500",
     },
     {
       icon: Library,
-      label: "Total Albums",
+      label: "Álbumes Totales",
       value: stats.totalAlbums.toString(),
       bgColor: "bg-violet-500/10",
       iconColor: "text-violet-500",
     },
     {
       icon: Users2,
-      label: "Total Artists",
+      label: "Artistas Totales",
       value: stats.totalArtists.toString(),
       bgColor: "bg-orange-500/10",
       iconColor: "text-orange-500",
     },
     {
       icon: PlayCircle,
-      label: "Total Users",
+      label: "Usuarios Totales",
       value: stats.totalUsers.toLocaleString(),
       bgColor: "bg-sky-500/10",
       iconColor: "text-sky-500",
