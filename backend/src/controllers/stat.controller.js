eb6e5d1b58590d66a4b8a70ef0be55259435f78f
KSAplay <kennysaavedraaltuna@outlook.com>
2025-03-09 01:01:53 +0100

    Agregar parámetro 'next' a la función getStats en stat.controller.js
    Signed-off-by: KSAplay <kennysaavedraaltuna@outlook.com>

--- backend/src/controllers/stat.controller.js
+++ backend/src/controllers/stat.controller.js
@@ -2,7 +2,7 @@ import { Song } from "../models/song.model.js";
 import { Album } from "../models/album.model.js";
 import { User } from "../models/user.model.js";
 
-export const getStats = async (req, res) => {
+export const getStats = async (req, res, next) => {
   try {
     const [totalSongs, totalUsers, totalAlbums, uniqueArtists] =
       await Promise.all([
