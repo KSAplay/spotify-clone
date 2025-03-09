08ff992cff951f5c1b5222bf40edfdd30c39a898
KSAplay <kennysaavedraaltuna@outlook.com>
2025-03-09 02:04:54 +0100

    Agregar respuesta JSON en el controlador getAllSongs para devolver la lista de canciones
    Signed-off-by: KSAplay <kennysaavedraaltuna@outlook.com>

--- backend/src/controllers/song.controller.js
+++ backend/src/controllers/song.controller.js
@@ -5,6 +5,8 @@ export const getAllSongs = async (req, res, next) => {
     // -1 = Descending => newest -> oldest
     // 1 = Ascending => oldest -> newest
     const songs = await Song.find().sort({ createdAt: -1 });
+
+    res.json(songs);
   } catch (error) {
     next(error);
   }
